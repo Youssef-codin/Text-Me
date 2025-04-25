3070b86a07046e1aac2966159053d8a846b3ab85
Joe-arch <youssef240100538@sut.edu.eg>
2025-04-25 19:48:06 +0300

    updated SQL script

--- database/script.sql
+++ database/script.sql
@@ -1,17 +1,23 @@
+--DROP TABLE users;
+--
 --CREATE TABLE users(
 --usr_name VARCHAR(40) PRIMARY KEY UNIQUE NOT NULL,
+--usr_email VARCHAR(255) UNIQUE NOT NULL,
 --usr_password VARCHAR(100) NOT NULL,
 --usr_salt VARCHAR(40) NOT NULL,
 --usr_key BLOB NOT NULL
 --);
 
-
+--
 --CREATE TABLE msgs(
 --msg_id INTEGER PRIMARY KEY,
---msg_sender VARCHAR(40) NOT NULL,
---msg_receiver VARCHAR(40) NOT NULL,
 --msg BLOB NOT NULL,
+--msg_sender VARCHAR(40) NOT NULL,
+--aes_sender BLOB NOT NULL,
+--msg_receiver VARCHAR(40) NOT NULL,
+--aes_receiver BLOB NOT NULL,
 --time_stamp INTEGER NOT NULL,
+--aes_iv BLOB NOT NULL,
 --FOREIGN KEY (msg_sender) REFERENCES users(usr_name),
 --FOREIGN KEY (msg_receiver) REFERENCES users(usr_name)
 --);
@@ -19,7 +25,7 @@
 
 --PRAGMA foreign_keys = on;
 
-DELETE FROM users;
-DELETE FROM msgs;
+--DELETE FROM users;
+--DELETE FROM msgs;
 SELECT * FROM users;
 SELECT * FROM msgs;
